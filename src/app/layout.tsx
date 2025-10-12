94473fc02761fff8045731f0ddb665962be37582
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-12 15:53:50 +0100

    Adding the header raw and testing the aceternity components - warming up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <FloatingNav
+          navItems={[
+            { name: "About", link: "/" },
+            { name: "Projects", link: "/" },
+            { name: "Testimonials", link: "/" },
+            { name: "Contact", link: "/" },
+          ]}
+        />
         {children}
       </body>
     </html>
