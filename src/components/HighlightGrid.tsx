3fa91f017c34d75078a16f86710691e8ffba5ff3
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-12 18:55:50 +0100

    correcting the gap between the boxes

--- src/components/HighlightGrid.tsx
+++ src/components/HighlightGrid.tsx
@@ -3,13 +3,13 @@ import { WobbleCard } from "./ui/wobble-card";
 
 export default function HighlightGrid() {
   return (
-    <div className="w-full flex flex-col gap-2 md:gap-5 py-5">
-      <div id="upper-grid" className="flex flex-col lg:flex-row w-full gap-2">
+    <div className="w-full flex flex-col gap-7 md:gap-7 py-5">
+      <div id="upper-grid" className="flex flex-col lg:flex-row w-full gap-7">
         <WobbleCard containerClassName="w-full lg:w-1/2">
           <h1>Card1</h1>
         </WobbleCard>
 
-        <div className="w-full lg:w-1/2 flex flex-col md:flex-row lg:flex-col gap-2">
+        <div className="w-full lg:w-1/2 flex flex-col md:flex-row lg:flex-col gap-7">
           <WobbleCard containerClassName="w-full md:w-1/2 lg:w-full">
             <h1>Card2</h1>
           </WobbleCard>
@@ -20,8 +20,8 @@ export default function HighlightGrid() {
         </div>
       </div>
 
-      <div id="lower-grid" className="flex flex-col md:flex-row w-full gap-2">
-        <div className="w-full md:w-1/2 flex flex-col gap-2">
+      <div id="lower-grid" className="flex flex-col md:flex-row w-full gap-7">
+        <div className="w-full md:w-1/2 flex flex-col gap-7">
           <WobbleCard containerClassName="w-full">
             <h1>Card4</h1>
           </WobbleCard>
