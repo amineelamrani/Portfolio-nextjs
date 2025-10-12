e386177bd729308ac2859f115372df1b6f3199a4
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-12 17:49:19 +0100

    Added the generation effect of the text and the button border effect in the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Hero from "@/components/Hero";
+import { Spotlight } from "@/components/ui/spotlight";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="container mx-auto flex flex-col items-center">
-      <Hero />
+    <div>
+      <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-30"
+        fill="white"
+      />
+      <div className="container mx-auto pt-25 flex flex-col items-center">
+        <Hero />
+      </div>
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Send } from "lucide-react";
-import { Spotlight } from "./ui/spotlight";
 import { cn } from "@/lib/utils";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export default function Hero() {
   return (
@@ -12,23 +12,28 @@ export default function Hero() {
           "[background-image:linear-gradient(to_right,#171717_2px,transparent_2px),linear-gradient(to_bottom,#171717_2px,transparent_1px)]"
         )}
       />
-      <Spotlight
-        className="-top-40 left-0 md:-top-20 md:left-60"
-        fill="white"
-      />
+
       <h3 className="text-xl text-gray-400 uppercase z-10">
         Dynamic Web Magic with Next.js
       </h3>
-      <h1 className="text-5xl font-bold z-10">
-        Transforming Concepts into Seamless{" "}
-        <span className="text-purple-300">User Experiences</span>
+      <h1 className="text-5xl font-bold z-10 text-white">
+        <TextGenerateEffect
+          className="text-white text-5xl font-bold z-10"
+          words={`Transforming Concepts into Seamless User Experiences`}
+          filter={false}
+        />
       </h1>
       <h2 className="text-2xl z-10">
         Hi! I&apos;m Amine, a MERN & Next.js Developer based in Morocco.
       </h2>
-      <button className="flex items-center gap-1 justify-center z-10">
-        <p>Show my work </p>
-        <Send size={10} />
+      <button className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          <div className="flex items-center gap-1 justify-center z-10">
+            <p>Show my work </p>
+            <Send size={10} />
+          </div>
+        </span>
       </button>
     </div>
   );
