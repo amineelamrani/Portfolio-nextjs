aece3de539e2e17bbe477e56c2b9713430a28de0
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-16 21:55:22 +0100

    Now the cards is as expected now just style it for the needed behaviour

--- src/components/WorkExperienceSection.tsx
+++ src/components/WorkExperienceSection.tsx
@@ -7,11 +7,13 @@ export default function WorkExperienceSection() {
       <h1 className="text-4xl font-bold text-center">
         My <span className="text-purple-300">work experience</span>
       </h1>
-      <div id="experiences-grid">
+      <div id="experiences-grid" className="w-full">
         <Button
           borderRadius="1.75rem"
-          className="border-slate-800"
-          borderClassName="h-70 bg-[radial-gradient(#593d78_100%,#593d78_50%)]"
+          className="bg-[rgb(4, 7, 29)] border-slate-800 backdrop-blur-xl "
+          borderClassName="h-30 w-20 rounded-full bg-[radial-gradient(#593d78_80%,#593d78_50%)] "
+          containerClassName="w-full h-50 "
+          duration={8000}
         >
           Borders are cool
         </Button>
