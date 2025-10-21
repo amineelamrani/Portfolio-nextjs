d6053c8175827bbad7f2d406362f9749051d83b7
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-21 19:01:54 +0100

    Improved the behaviour of the projects on the different screens

--- src/components/customComponents/ProjectsCard.tsx
+++ src/components/customComponents/ProjectsCard.tsx
@@ -20,52 +20,54 @@ export default function ProjectsCard({
 }: ComponentProps) {
   return (
     <div className="w-full lg:w-1/2 relative px-5 py-2 lg:min-w-[450px]">
-      <PinContainer
-        title="Visit"
-        href={url}
-        containerClassName="w-full h-full p-0 top-0 bg-transparent"
-        className="flex flex-col w-full gap-5 p-0 top-0 bg-transparent"
-      >
-        <div className="w-full  bg-[url('/bg.png')] bg-cover bg-center flex justify-center items-center pt-15 overflow-hidden">
-          <Image
-            src={image}
-            alt={title}
-            width={1000}
-            height={800}
-            className="w-4/5 rotate-6 translate-y-6 rounded-t-xl"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <h1 className="text-base sm:text-xl md:text-2xl font-bold py-2">
-            {title}
-          </h1>
-          <p className="text-gray-400 text-xs sm:text-sm md:text-base">
-            {description}
-          </p>
-        </div>
-        <div className="w-full flex justify-between">
-          <div className="flex gap-0">
-            {techStack.map((tech, index) => (
-              <div
-                key={`${tech}-${index}-tooltip-imagetechstack`}
-                className={`bg-[rgb(9,0,31)] -mr-2 w-8 h-8 sm:w-10 sm:h-10 rounded-full border-gray-500 border-[0.2px] flex items-center justify-center`}
-              >
-                <Image
-                  src={tech}
-                  width={50}
-                  height={50}
-                  className="w-3 h-3 sm:w-5 sm:h-5"
-                  alt={`tech-${tech}`}
-                />
-              </div>
-            ))}
+      <a href={url} target={"_blank"}>
+        <PinContainer
+          title="Visit"
+          href={url}
+          containerClassName="w-full h-full p-0 top-0 bg-transparent"
+          className="flex flex-col w-full gap-5 p-0 top-0 bg-transparent"
+        >
+          <div className="w-full  bg-[url('/bg.png')] bg-cover bg-center flex justify-center items-center pt-15 overflow-hidden">
+            <Image
+              src={image}
+              alt={title}
+              width={1000}
+              height={800}
+              className="w-4/5 rotate-6 translate-y-6 rounded-t-xl"
+            />
           </div>
-          <p className="text-purple-300 font-bold text-sm sm:text-lg md:text-xl flex gap-1 items-center">
-            Check Live Site <ArrowUpRight />
-          </p>
-        </div>
-      </PinContainer>
+
+          <div className="flex flex-col">
+            <h1 className="text-base sm:text-xl md:text-2xl font-bold py-2">
+              {title}
+            </h1>
+            <p className="text-gray-400 text-xs sm:text-sm md:text-base">
+              {description}
+            </p>
+          </div>
+          <div className="w-full flex justify-between">
+            <div className="flex gap-0">
+              {techStack.map((tech, index) => (
+                <div
+                  key={`${tech}-${index}-tooltip-imagetechstack`}
+                  className={`bg-[rgb(9,0,31)] -mr-2 w-8 h-8 sm:w-10 sm:h-10 rounded-full border-gray-500 border-[0.2px] flex items-center justify-center`}
+                >
+                  <Image
+                    src={tech}
+                    width={50}
+                    height={50}
+                    className="w-3 h-3 sm:w-5 sm:h-5"
+                    alt={`tech-${tech}`}
+                  />
+                </div>
+              ))}
+            </div>
+            <p className="text-purple-300 font-bold text-sm sm:text-lg md:text-xl flex gap-1 items-center">
+              Check Live Site <ArrowUpRight />
+            </p>
+          </div>
+        </PinContainer>
+      </a>
     </div>
   );
 }
--- src/components/ui/3d-pin.tsx
+++ src/components/ui/3d-pin.tsx
@@ -63,20 +63,16 @@ export const PinPerspective = ({
   href?: string;
 }) => {
   return (
-    <motion.div className="pointer-events-none flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+    <motion.div className=" flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full flex-none  inset-0">
-        <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
-          >
+        <div className="absolute top-0 inset-x-0 flex justify-center">
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
@@ -151,10 +147,10 @@ export const PinPerspective = ({
         </div>
 
         <>
-          <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40 blur-[2px]" />
-          <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40  " />
-          <motion.div className="absolute right-1/2 translate-x-[1.5px] bottom-1/2 bg-cyan-600 translate-y-[14px] w-[4px] h-[4px] rounded-full z-40 blur-[3px]" />
-          <motion.div className="absolute right-1/2 translate-x-[0.5px] bottom-1/2 bg-cyan-300 translate-y-[14px] w-[2px] h-[2px] rounded-full z-40 " />
+          <motion.div className="block absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40 blur-[2px]" />
+          <motion.div className="block absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40  " />
+          <motion.div className="block absolute right-1/2 translate-x-[1.5px] bottom-1/2 bg-cyan-600 translate-y-[14px] w-[4px] h-[4px] rounded-full z-40 blur-[3px]" />
+          <motion.div className="block absolute right-1/2 translate-x-[0.5px] bottom-1/2 bg-cyan-300 translate-y-[14px] w-[2px] h-[2px] rounded-full z-40 " />
         </>
       </div>
     </motion.div>
