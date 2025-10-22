9615d501e66b5cdc4b2b5f9ecb65616f4efeeb7c
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-10-22 17:43:36 +0100

    Making the navbar shown at startup and at the top of the screen scroll

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -21,7 +21,7 @@ export const FloatingNav = ({
 }) => {
   const { scrollYProgress } = useScroll();
 
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
@@ -29,8 +29,8 @@ export const FloatingNav = ({
       // eslint-disable-next-line prefer-const
       let direction = current! - scrollYProgress.getPrevious()!;
 
-      if (scrollYProgress.get() < 0.05) {
-        setVisible(false);
+      if (scrollYProgress.get() < 0.001) {
+        setVisible(true);
       } else {
         if (direction < 0) {
           setVisible(true);
@@ -53,10 +53,11 @@ export const FloatingNav = ({
           opacity: visible ? 1 : 0,
         }}
         transition={{
-          duration: 0.2,
+          duration: 0.5,
+          ease: "easeInOut",
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-4  items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 sm:px-10 py-4  items-center justify-center space-x-4",
           className
         )}
       >
@@ -69,8 +70,7 @@ export const FloatingNav = ({
               "relative items-center flex space-x-1 hover:text-gray-400"
             )}
           >
-            <span className="hidden">{navItem.icon}</span>
-            <span className="block text-xs sm:text-sm">{navItem.name}</span>
+            <span className="text-xs sm:text-sm">{navItem.name}</span>
           </a>
         ))}
       </motion.div>
